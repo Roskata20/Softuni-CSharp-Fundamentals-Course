7b7c1ac07627ddf5722c00e699971ea830e91427
Rosen Yordanov <rosensy2003@gmail.com>
2022-06-14 12:12:41 +0300

    Add project files.

---
+++ Data Types/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Data_Types
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
